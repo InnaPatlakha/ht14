77013ff1c18d4c39bd77930e1326e4fd04036b23
Inna Patlakha <i.inna1903@gmail.com>
2023-07-27 22:37:56 +0300

    task done

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
+Array.prototype.remove = function(value) {
+  this.splice(this.indexOf(value), 1);
+}
+
+let array = ['tree', 'bird', 'rain', 'book', 'traveling', 'blue', 'enough'];
+let item = 'blue';
+array.remove(item);
+console.log(array);
